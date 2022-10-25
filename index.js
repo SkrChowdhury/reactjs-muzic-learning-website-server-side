0b2f7a532c54eef1928c6ef30943dcce68f54970
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-26 01:27:25 +0600

    json implemented done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require("./data/courses.json");
+const course = require("./data/course.json");
+
+app.get("/", (req, res) => {
+  res.send("Muzic API Running");
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const category_courses = course.filter((n) => n.category_id === id);
+  res.send(category_courses);
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+
+app.listen(port, () => {
+  console.log("Muzic Server running on port", port);
+});
