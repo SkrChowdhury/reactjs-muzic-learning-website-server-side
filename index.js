e429d26b01e24ee2a454abb0f5ef3371cca5b7e4
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-26 21:56:59 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require("./data/courses.json");
-const course = require("./data/course.json");
+const courses = require('./data/courses.json');
+const course = require('./data/course.json');
 
-app.get("/", (req, res) => {
-  res.send("Muzic API Running");
+app.get('/', (req, res) => {
+  res.send('Muzic API Running');
 });
 
-app.get("/courses/:id", (req, res) => {
+app.get('/courses/:id', (req, res) => {
   const id = req.params.id;
   const category_courses = course.filter((n) => n.category_id === id);
   res.send(category_courses);
 });
 
-app.get("/courses", (req, res) => {
+app.get('/courses', (req, res) => {
   res.send(courses);
 });
 
-
 app.listen(port, () => {
-  console.log("Muzic Server running on port", port);
+  console.log('Muzic Server running on port', port);
 });
