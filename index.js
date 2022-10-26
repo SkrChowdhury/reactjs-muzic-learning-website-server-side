6b05627b5fc4ab2cd7614a677319ba15cbbf87ba
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-26 22:36:13 +0600

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port  =  process.env.PORT || 5000;
 
 app.use(cors());
 
 const courses = require('./data/courses.json');
-const course = require('./data/course.json');
+const course  = require('./data/course.json');
 
 app.get('/', (req, res) => {
   res.send('Muzic API Running');
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 app.get('/courses/:id', (req, res) => {
   const id = req.params.id;
   const category_courses = course.filter((n) => n.category_id === id);
-  res.send(category_courses);
+  res.send(category_courses); 
 });
 
 app.get('/courses', (req, res) => {
@@ -23,5 +23,5 @@ app.get('/courses', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Muzic Server running on port', port);
+  console.log('Muzic Server running on port', port); 
 });
